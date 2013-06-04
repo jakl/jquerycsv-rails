d0173506e0e439e4dd0895b34bdd093c7e8b806b
James Koval <james.ross.koval@gmail.com>
2013-06-04 16:36:21 -0700

    initial commit

---
+++ lib/jquerycsv/rails.rb
@@ -0,0 +1,7 @@
+require "jquerycsv/rails/version"
+
+module Jquerycsv
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/jquerycsv/rails/version.rb
@@ -0,0 +1,5 @@
+module Jquerycsv
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
